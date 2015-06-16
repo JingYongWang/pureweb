e93de9eadcdd3870848bf0de454195b8b57046a7
mac001 <mac001@nqu-192-168-61-131.internal>
2015-06-16 12:01:58 +0800

    server.js add

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 var koa = require('koa');
 var app = koa();
 var crouter = require('koa-router'); 
+var router = crouter();
 // var crouter = require('koa-router')()
 //()為一個建構函數
 var logger = require('koa-logger');
-var router = crouter();
+var server = require('koa-static');
 //var view = require('co-view');
 //var body = require('co-body');
 
 app.use(router.routes())
 app.use(router.allowedMethods());
-
+app.use(server(./web));
 
 router.get('/', function *(next){ 
 	this.body = 'Hello World';
