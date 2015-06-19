61d1d08e845d18fab0c8be18d85b5cd84b898920
JingYongWang <lovedpotato@gmail.com>
2015-06-20 02:44:05 +0800

    login.html & server

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ var route = require('koa-route')
 var mount = require('koa-mount'); 
 var logger = require('koa-logger');
 var server = require('koa-static-folder');
-
+var co      = require('co');
+var monk    = require('monk');
+var comonk  = require('co-monk');
+var db      = monk('localhost/test');
+var password   = comonk(db.get('password'));
 
 //var view = require('co-view');
 //var body = require('co-body');
@@ -16,6 +20,27 @@ app.use(server('./web'));
 app.use(route.get('/', function *(){
 	this.redirect('/web/pure.html');
 	
+}));
+
+app.use(route.post("/password", function *login() {
+  var req = this.request,
+      res = this.response;
+  var pd = this.request.body.password;
+  var user = this.request.body.user;
+  console.log("/password path=%s", this.path);
+  var objs = yield password.find({}); 
+
+  if(objs[0].passwordList[0].password == pd)
+	   {
+	      if(objs[0].passwordList[0].user == user)
+		  {
+		     response(this.response, 200, "學生登入成功");
+		  }else{
+		     response(this.response, 404, "登入失敗!!");
+		  }
+	   }else{
+	       response(this.response, 200, "登入失敗!!");  
+	   }
 }));
 
   app.listen(3100);
